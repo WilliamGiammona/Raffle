108ed2f5e364d55f90ca7d0664d386ed0925877e
William Giammona <wgiammona@gmail.com>
2022-12-06 13:07:53 +0200

    updated how ABI is written to front end

--- deploy/99-deploy-frontEnd.ts
+++ deploy/99-deploy-frontEnd.ts
@@ -1,4 +1,3 @@
-import "dotenv/config";
 import { ethers, network } from "hardhat";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
@@ -6,7 +5,7 @@ import { DeployFunction } from "hardhat-deploy/types";
 import * as fs from "fs";
 
 const FRONT_END_ADDRESSES_FILE = "../raffle-front-end/constants/contractAddresses.json";
-const FRONT_END_ABI_FILE = "../raffle-front-end/constants/abi.json";
+const FRONT_END_ABI_FILE = "../raffle-front-end/constants/contractABIs.json";
 const deployFrontEnd = async function () {
     if (process.env.UPDATE_FRONT_END === "true") {
         console.log("Updating Front End");
@@ -18,7 +17,7 @@ const deployFrontEnd = async function () {
 };
 
 async function updateContractAddresses() {
-    const chainId = network.config.chainId?.toString()!;
+    const chainId = network.config.chainId!.toString();
     const Raffle = await ethers.getContract("Raffle");
     const contractAddresses = JSON.parse(fs.readFileSync(FRONT_END_ADDRESSES_FILE, "utf-8"));
     if (chainId! in contractAddresses) {
@@ -34,8 +33,17 @@ async function updateContractAddresses() {
 
 async function updateABI() {
     const Raffle = await ethers.getContract("Raffle");
-    const typeFix: any = Raffle.interface.format(ethers.utils.FormatTypes.json);
-    fs.writeFileSync(FRONT_END_ABI_FILE, typeFix);
+    const contractName = "Raffle";
+    console.log(contractName);
+    const contractAbis = JSON.parse(fs.readFileSync(FRONT_END_ABI_FILE, "utf-8"));
+    if (contractName in contractAbis) {
+        if (!contractAbis[contractName].includes(Raffle.interface.format(ethers.utils.FormatTypes.json))) {
+            contractAbis[contractName].push(Raffle.interface.format(ethers.utils.FormatTypes.json));
+        }
+    } else {
+        contractAbis[contractName] = [Raffle.interface.format(ethers.utils.FormatTypes.json)];
+    }
+    fs.writeFileSync(FRONT_END_ABI_FILE, JSON.stringify(contractAbis));
 }
 
 export default deployFrontEnd;
