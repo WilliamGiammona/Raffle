ee2a14c813adf42fae39047c6593ff330d0f9ab9
William Giammona <wgiammona@gmail.com>
2022-12-07 11:37:24 +0200

    Added logic for mockVRF

--- scripts/mockOffChain.ts
+++ scripts/mockOffChain.ts
@@ -1,19 +1,34 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
+import { BigNumber } from "ethers";
+import { Raffle, VRFCoordinatorV2Mock } from "../typechain-types";
 
-async function main() {
+async function mockRequestRandomWords() {
     const accounts = await ethers.getSigners();
     let deployer = accounts[0];
     let player1 = accounts[1];
-    const MyContract = await ethers.getContract("MyContract", deployer);
-    console.log(`Got contract MyContract at ${MyContract.address}`);
-    console.log("Initiating Transaction...");
-    const txResponse = await MyContract.method({});
+    const Raffle: Raffle = await ethers.getContract("Raffle", deployer);
+    const txResponse = await Raffle.requestRandomWords();
     const txReceipt = await txResponse.wait(1);
+    const requestId = await Raffle.getLastRequestId();
+    if (network.config.chainId == 31337) {
+        await mockVrf(requestId, Raffle);
+    } else {
+        console.log("No need to mock, we're on a real network");
+    }
     console.log("Transaction finished!!!");
     console.log("-------------------------------------------------------------");
 }
 
-main()
+async function mockVrf(requestId: BigNumber, raffle: Raffle) {
+    console.log("We on a local network? Ok let's pretend...");
+    const vrfCoordinatorV2Mock: VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+    await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, raffle.address);
+    console.log("Responded!");
+    const recentWinner = await raffle.getWinner();
+    console.log(`The winner is: ${recentWinner}`);
+}
+
+mockRequestRandomWords()
     .then(() => process.exit(0))
     .catch((error) => {
         console.error(error);
